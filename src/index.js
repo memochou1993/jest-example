d3f6522612b9a3fa467a2ec02c2fab725b1d22ff
William Chou <williamchou@kklab.com>
2022-09-16 15:37:17 +0800

    Use sample test case

--- src/index.js
+++ src/index.js
@@ -1,57 +1,9 @@
 const products = require('./products');
 
 const practice = {
-  case9() {
+  case0() {
     const res = products
-      .every((product) => product.images.length > 0);
-    return res;
-  },
-  case10() {
-    const res = products
-      .some((product) => product.description.includes('no side effects'));
-    return res;
-  },
-  case11() {
-    const res = products
-      .filter((product) => product.category === 'smartphones')
-      .length;
-    return res;
-  },
-  case12() {
-    const res = products
-      .filter((product) => product.rating > 4.9)
-      .map((product) => ({
-        brand: product.brand,
-      }));
-    return res;
-  },
-  case13() {
-    const res = products
-      .find((product) => product.brand === 'Dry Rose')
-      .title;
-    return res;
-  },
-  case14() {
-    const res = products
-      .reduce((a, b) => a + b.price * b.stock, 0);
-    return res;
-  },
-  case15() {
-    const res = products
-      .sort((a, b) => b.discountPercentage - a.discountPercentage)
-      .map((p) => p.brand)
-      .slice(0, 3);
-    return res;
-  },
-  case16() {
-    const brands = {};
-    products
-      .forEach((p) => {
-        if (!brands[p.brand]) {
-          brands[p.brand] = true;
-        }
-      });
-    const res= Object.keys(brands).length;
+      .every((product) => product.price < 5000);
     return res;
   },
 };
--- src/index.test.js
+++ src/index.test.js
@@ -1,57 +1,8 @@
 const practice = require('.');
 
-test('case9 should determine that all products have one image at least', () => {
-  const actual = practice.case9();
+test('case0 should determine all product prices are less than 5000',  () => {
+  const actual = practice.case0();
   const expected = true;
-
-  expect(actual).toStrictEqual(expected);
-});
-
-test('case10 should determine that there is a product which description says "no side effects"', () => {
-  const actual = practice.case10();
-  const expected = true;
-
-  expect(actual).toStrictEqual(expected);
-});
-
-test('case11 should get the number of smartphones', () => {
-  const actual = practice.case11();
-  const expected = 5;
-
-  expect(actual).toStrictEqual(expected);
-});
-
-test('case12 should get the brand of products which get over 4.9 rating', () => {
-  const actual = practice.case12();
-  const expected = [{ brand: 'fauji' }, { brand: 'Golden' }];
-
-  expect(actual).toStrictEqual(expected);
-});
-
-test('case13 should get the only product of Dry Rose', () => {
-  const actual = practice.case13();
-  const expected = 'Gulab Powder 50 Gram';
-
-  expect(actual).toStrictEqual(expected);
-});
-
-test('case14 should get the total revenue when all the products sold without discount', () => {
-  const actual = practice.case14();
-  const expected = 765200;
-
-  expect(actual).toStrictEqual(expected);
-});
-
-test('case15 should get the top 3 highest discounted product brands',  () => {
-  const actual = practice.case15();
-  const expected = ['Apple', 'OPPO', 'Boho Decor'];
-
-  expect(actual).toStrictEqual(expected);
-});
-
-test('case16 should get the number of product brands',  () => {
-  const actual = practice.case16();
-  const expected = 28;
 
   expect(actual).toStrictEqual(expected);
 });
